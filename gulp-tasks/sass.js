f2e8f719c2553d0646a244a2dbf19ea9006d32e4
Rizaldi Martaputra <rizaldi.m@gmail.com>
2021-03-11 18:01:35 +0700

    Create new directory for gulp tasks
    and add sass.js to it. It's for sass task. What it does basically are:
    
    - Pipe file in src/scss to sass processor, then
    - Clean-css it, then
    - Copy it to src/_includes/css

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -5,3 +5,16 @@ const sassProcessor = require('gulp-sass');
 // We want to be using canonical Sass, rather than node-sass
 sassProcessor.compiler = require('sass');
 
+const sass = () => {
+	return src('./src/scss/*.scss')
+		.pipe(sassProcessor().on('error', sassProcessor.logError))
+		.pipe( cleanCSS( { level: 2 } ) )
+		.pipe(
+			dest(
+				'./src/_includes/css',
+				{ sourceMaps: true }
+			)
+		);
+};
+
+module.exports = sass;
